2b0ba5107d908062f463b8cc890f8e6469ec7894
Jeff Lyon <jeff@albatrossdigital.com>
2015-11-19 13:03:31 -0800

    Refactoring for new proud_aggregation module

--- modules/proud_social_app/templates/app.tpl.php
+++
@@ -1,5 +0,0 @@
-<div id="social-app">
-  <div social-<?php print $conf['widget_type']; ?> 
-       social-post-count="<?php print $conf['post_count']; ?>" 
-       social-hide-controls="<?php print $conf['show_controls']; ?>"></div>
-</div>
